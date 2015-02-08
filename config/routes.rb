aab5af358377c62b126d6d0af25f00d87d1b20a0
wlans <wyatt.lansdale@gmail.com>
2015-02-07 19:08:17 -0600

    Finish log in/out

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,6 @@
 Rails.application.routes.draw do
 
-
-  get 'users/new'
+  root 'static_pages#home'
 
   get 'help' => 'static_pages#help'
 
@@ -11,13 +10,19 @@ Rails.application.routes.draw do
 
   get 'signup' => 'users#new'
 
+  get 'login' => 'sessions#new'
+
+  post 'login' => 'sessions#create'
+
+  delete 'logout' => 'sessions#destroy'
+
   resources :users
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
-  root 'static_pages#home'
+  
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -55,8 +55,11 @@ class UserTest < ActiveSupport::TestCase
 	test "password should have a minimum length" do 
 		@user.password = @user.password_confirmation = "a" * 5
 		assert_not @user.valid?
+	end
+
+	test "authenticated? should return false for a user with nil digets" do 	
+		assert_not @user.authenticated?('')
 	end	
-		
 
 
 
