0f8f237300edc6659da47367894dc6895eed8bb6
wlans <wyatt.lansdale@gmail.com>
2015-03-16 14:51:54 -0500

    Fixed everything

--- db/schema.rb
+++ db/schema.rb
@@ -13,7 +13,21 @@
 
 ActiveRecord::Schema.define(version: 20150316192328) do
 
-# Could not dump table "Users" because of following NoMethodError
-#   undefined method `[]' for nil:NilClass
+  create_table "users", force: true do |t|
+    t.string   "name"
+    t.string   "email"
+    t.datetime "created_at",                        null: false
+    t.datetime "updated_at",                        null: false
+    t.string   "password_digest"
+    t.string   "remember_digest"
+    t.boolean  "admin",             default: false
+    t.string   "activation_digest"
+    t.boolean  "activated",         default: false
+    t.datetime "activated_at"
+    t.string   "reset_digest"
+    t.datetime "reset_sent_at"
+  end
+
+  add_index "users", ["email"], name: "index_users_on_email", unique: true
 
 end
