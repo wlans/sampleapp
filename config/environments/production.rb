9af9dd2f8673d488c1c6fad52aea3dcc83ce7e5d
wlans <wyatt.lansdale@gmail.com>
2015-03-16 10:12:28 -0500

    fixed bug with heroku

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -76,7 +76,7 @@ Rails.application.configure do
   config.active_record.dump_schema_after_migration = false
 
   config.action_mailer.raise_delivery_errors = true
-  config.action_mailer.delivery_method = :smtp
+  
   host = 'stormy-anchorage-3744.herokuapp.com'
   config.action_mailer.default_url_options = {host: host}
   ActionMailer::Base.smtp_setting = {
